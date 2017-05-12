274b3ad69153f0dd8df6617e1ec9c59b572b0c0f
Abhishek Gupta <abhirockzz@gmail.com>
2017-05-12 21:18:59 +0530

    Update JedisProducer.java

--- src/main/java/com/wordpress/abhirockzz/redis/cdi/JedisProducer.java
+++ src/main/java/com/wordpress/abhirockzz/redis/cdi/JedisProducer.java
@@ -12,21 +12,10 @@ import redis.clients.jedis.Jedis;
 @ApplicationScoped
 public class JedisProducer {
 
-    @PostConstruct
-    public void init() {
-        System.out.println("JedisProducer init()");
-    }
-
-    @PreDestroy
-    public void byebye() {
-        System.out.println("JedisProducer destroy()");
-    }
-
     @Dependent
     @Produces
     public Jedis get(InjectionPoint ip) {
         System.out.println("injecting Jedis into " + ip.getMember().getDeclaringClass().getSimpleName());
-        //String redisHost = System.getProperty("REDIS_HOST", "192.168.99.100");
         String redisHost = System.getenv().getOrDefault("REDIS_HOST", "192.168.99.100");
         String redisPort = System.getenv().getOrDefault("REDIS_PORT", "6379");
 
